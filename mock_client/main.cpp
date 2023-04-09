755453d19e56f8cc029300e658af1414d199b4ff
davide99 <info.davide99@gmail.com>
2023-04-09 21:04:31 +0200

    Ora mock client cerca nel db

--- mock_client/main.cpp
+++ mock_client/main.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <fin/readers/wav_reader.h>
 #include <fin/fin.h>
+#include <fin_db/fin_db.h>
+#include <fin_db/db.h>
+
+#define TO_FILE
 
 int main(int argc, char **argv) {
     if (argc != 2) return -1;
@@ -10,14 +14,21 @@ int main(int argc, char **argv) {
     auto links = fin::computeLinks(reader);
     reader.dropSamples();
 
-    std::string json = "[";
-    for (auto &link: links) {
-        json += "{\"hash\":" + std::to_string(link.getHash()) +
-                ",\"window\":" + std::to_string(link.getTime()) + "},";
-    }
-    json.pop_back();
-    json += "]";
+    fin::utils::ByteBuffer buf = links.toByteBuffer();
 
-    std::cout << json << std::endl;
+#ifdef TO_FILE
+    FILE* pFile;
+    pFile = fopen("file.binary", "wb");
+    fwrite(buf.getData(), 1, buf.getSize(), pFile);
+    fclose(pFile);
+#endif
+
+    auto links_out = fin::core::Links::fromByteBuffer(buf);
+
+    fin::DB db;
+    std::string song = fin::searchFromLinks(links, db);
+
+    std::cout << song << std::endl;
+
     return 0;
 }
