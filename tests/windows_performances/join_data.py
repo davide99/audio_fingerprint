35fa86cde2c40ad00a43e41d9146c4d18ba25a2c
davide99 <info.davide99@gmail.com>
2023-04-15 11:31:41 +0200

    Aggiunto riscalamento

--- tests/windows_performances/join_data.py
+++ tests/windows_performances/join_data.py
@@ -2,6 +2,7 @@ import glob
 
 if __name__ == '__main__':
     values = {}
+    maxes = {}
 
     for file in glob.glob('data_*.csv'):
         window_name = file[5:-4]
@@ -10,12 +11,16 @@ if __name__ == '__main__':
             for line in f:
                 splitted_line = line.strip().split(",")
                 db = splitted_line[0]
-                rate = splitted_line[1]
+                rate = float(splitted_line[1])
 
                 if db not in values:
                     values[db] = {}
 
                 values[db][window_name] = rate
+                if window_name not in maxes:
+                    maxes[window_name] = rate
+                elif maxes[window_name] < rate:
+                    maxes[window_name] = rate
 
     with open("../../doc/data/windows_success_rate.csv", "w") as f:
         first = True
@@ -30,5 +35,8 @@ if __name__ == '__main__':
 
             f.write(db)
             for window_name, _ in sorted(values[db].items()):
-                f.write(f",{values[db][window_name]}")
+                value = float(values[db][window_name])
+                value /= maxes[window_name]
+                value *= 100
+                f.write(f",{value}")
             f.write("\n")
