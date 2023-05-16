06dcfe46abc20d1b24225760bb98bbd021c5e230
davide99 <info.davide99@gmail.com>
2023-05-16 13:39:42 +0200

    Spostati file da template a static, fix cors

--- lyrics/main.py
+++ lyrics/main.py
@@ -15,7 +15,19 @@ def lyrics(song_id: int):
 
 @app.route("/")
 def home():
-    return render_template("subs.html")
+    return app.send_static_file("lyrics.html")
+
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return app.send_static_file(filename)
+
+
+@app.after_request
+def add_cors_header(response):
+    response.headers['Cross-Origin-Embedder-Policy'] = 'require-corp'
+    response.headers['Cross-Origin-Opener-Policy'] = 'same-origin'
+    return response
 
 
 if __name__ == "__main__":
