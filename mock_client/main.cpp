0def50c4ccf82488c057aede80a5445922243fab
davide99 <info.davide99@gmail.com>
2023-06-09 20:13:30 +0200

    Aggiunta stampa json a mock_client

--- mock_client/main.cpp
+++ mock_client/main.cpp
@@ -8,7 +8,7 @@
 //#define TO_FILE
 
 int main(int argc, char **argv) {
-    if (argc != 2) return -1;
+    if (argc != 2 && argc != 3) return -1;
 
     fin::readers::WavReader reader(argv[1]);
     auto links = fin::computeLinks(reader);
@@ -27,6 +27,18 @@ int main(int argc, char **argv) {
 
     fin::DB db;
     auto result = fin::searchFromLinks(links, db, true);
+
+    if (argc == 3 && std::string(argv[2]) == "--to-json") {
+        std::cout << "{"
+                  << "\"found\":" << (result.found ? "true" : "false") << ","
+                  << "\"id\":" << result.id << ","
+                  << "\"timeDelta\":" << result.timeDelta << ","
+                  << "\"commonLinks\":" << result.commonLinks << ","
+                  << "\"name\":\"" << result.name << "\""
+                  << "}\n";
+
+        return 0;
+    }
 
     if (result.found) {
         std::cout << result.name << std::endl;
