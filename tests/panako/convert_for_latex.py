dfc7d4f288cc0398406b8c12e88ff642aed9f8f6
davide99 <info.davide99@gmail.com>
2023-06-12 20:33:24 +0200

    Aggiunti dati panako in cartella lyx

--- tests/panako/convert_for_latex.py
+++ tests/panako/convert_for_latex.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    with open("../../doc/data/recording_length_success_rate.csv", "w") as w:
+    with open("../../doc/data/panako_length_success_rate.csv", "w") as w:
         w.write("snr,length,percentage_true,percentage_false\n")
 
         with open("data.csv", "r") as r:
